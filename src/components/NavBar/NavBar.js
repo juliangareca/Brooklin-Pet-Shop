e30cc0e381133727eca9b41b42935eb3f10431a2
Julian Gareca Kandrachoff <jugakan@gmail.com>
2022-05-17 20:40:29 -0300

    Modifico el NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const NavBar = () => {
+
+
+
   return (
 
     <nav className="navbar navbar-expand-lg bg-light">
@@ -15,14 +18,24 @@ const NavBar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Catalogo</a>
+              <a className="nav-link active" aria-current="page" href="#">Inicio</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Nosotros</a>
             </li>
+            
+            <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Catalogo
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Juguetes</a></li>
+            <li><a className="dropdown-item" href="#">Belleza </a></li>
+            <li><a className="dropdown-item" href="#">Salud </a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Rascadores</a></li>
+          </ul>
+        </li>
           </ul>
           <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
