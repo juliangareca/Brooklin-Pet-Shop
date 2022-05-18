b8d6305f7d1816ebc753792bfe6f7c18f6bb66dc
Julian Gareca Kandrachoff <jugakan@gmail.com>
2022-05-18 12:17:43 -0300

    Dark NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
 
   return (
 
-    <nav className="navbar navbar-expand-lg bg-light">
+    <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">Brooklin Pet Shop</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
