f34451264dffa04d322f9f9f2c8a67c820252c69
Julian Gareca Kandrachoff <jugakan@gmail.com>
2022-07-06 22:14:42 -0300

    Agrego GIF

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBLAuSkwwPO_jnlN1IxDUd4yhFtPCTul9s",
   authDomain: "brookling-pet-shop.firebaseapp.com",
@@ -14,9 +10,7 @@ const firebaseConfig = {
   appId: "1:232428218404:web:436c8a1ad3fccf99e610b1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Firestore initialize
 const db = getFirestore(app)
 
 
