0b8a175e9e45f3a2310bdaadea1e24f703981039
Julian Gareca Kandrachoff <jugakan@gmail.com>
2022-07-06 22:11:11 -0300

    Agrego GIF

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -12,7 +12,7 @@ const AddToCart = ({ product, count }) => {
     // console.log("esto viene de AddToCart: ", product)
     return (
         <>
-            <button onClick={() => {addProductToCart(product, count)}} type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+            <button onClick={() => { addProductToCart(product, count) }} type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">
                 Add Products
             </button>
 
@@ -20,7 +20,7 @@ const AddToCart = ({ product, count }) => {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                        {cartListItems.map((product) => {
+                            {cartListItems.map((product) => {
                                 return (
 
                                     <div className="card" style={{ flexFlow: "nowrap", marginBottom: "10px" }} key={product.id}>
@@ -30,7 +30,7 @@ const AddToCart = ({ product, count }) => {
                                             <p className="card-text">{product.precio}</p>
                                         </div>
 
-                                        <button onClick={() => {deleteItem(product.id)}} type="button" className="btn btn-danger"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
+                                        <button onClick={() => { deleteItem(product.id) }} type="button" className="btn btn-danger"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
                                             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
                                         </svg></button>
                                     </div>
--- src/components/Carrito/CartWidget.js
+++ src/components/Carrito/CartWidget.js
@@ -7,7 +7,7 @@ import './CartWidget.css';
 const Carrito = () => {
 
 
-    const { cartListItems, deleteItem, cleanCart,  totalCart2} = useContext(CartContext)
+    const { cartListItems, deleteItem, cleanCart, totalCart2 } = useContext(CartContext)
 
     if (cartListItems.length !== 0) {
 
@@ -15,15 +15,15 @@ const Carrito = () => {
         return (
 
             <div className="contenedor">
-                
+
                 <div className="nav-item">
 
-                <svg xmlns="http://www.w3.org/2000/svg" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" width="20" height="20" color="white" fill="currentColor" className="bi bi-cart-fill " viewBox="0 0 16 16">
-                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                </svg>
-                 <span className="badge bg-secondary" data-bs-target="#exampleModal" type="button">{ totalCart2() }</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" width="20" height="20" color="white" fill="currentColor" className="bi bi-cart-fill " viewBox="0 0 16 16">
+                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </svg>
+                    <span className="badge bg-secondary" data-bs-target="#exampleModal" type="button">{totalCart2()}</span>
 
-                 </div>
+                </div>
                 <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div className="modal-dialog">
                         <div className="modal-content">
@@ -65,8 +65,8 @@ const Carrito = () => {
         )
     }
 
-    else{
-        return(
+    else {
+        return (
             <h6>.</h6>
         )
     }
